66c94edfbffac5472d41c1f37c857ab135c782c2
Xavier <xavier.munoz.pisco@gmail.com>
2023-02-08 20:52:33 -0500

    Configurando propiedades y perfiles de spring boot

--- Softvier-tienda/Softvier-tienda/src/main/java/com/example/Softviertienda/web/controller/HolaMundoControlador.java
+++ Softvier-tienda/Softvier-tienda/src/main/java/com/example/Softviertienda/web/controller/HolaMundoControlador.java
@@ -1,4 +1,4 @@
-package com.example.Softviertienda;
+package com.example.Softviertienda.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
